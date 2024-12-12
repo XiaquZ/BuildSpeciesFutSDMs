dc9a83136769c9a05e8eba2ad5092bb8d9db6c05
XiaquZ <xiaqu.zhou@gmail.com>
2024-12-12 16:37:21 +0100

    Build targets pipeline.

---
+++ R/BuildVRTmaps.R
@@ -0,0 +1,22 @@
+
+Build_VRT <- function(tile_paths){
+  
+}
+
+species_tiles <- list.files("D:/Output/fut_SDMs/species_tiles/", full.names = T)
+species_names <- basename(species_tiles)
+
+for (species in species_names) {
+  t.list <- list.files(
+    species_tiles[grepl(species, basename(species_tiles))],
+    full.names = T
+  )
+  r <- vrt(t.list)
+  r <- round(r, digits = 1)
+  print(r)
+  fout <- paste0(
+    "D:/Output/fut_SDMs/species_final/",
+    species, "_predictedSDMs_2071-2100_ssp370_25m.tif"
+  )
+  writeRaster(r, fout, overwrite = TRUE)
+}
